1877a4c5f3160bf1291890bbd614317230bc7241
LaupWing <loc_strict@hotmail.com>
2022-07-11 20:42:39 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
    const {address, connectWallet} = useWeb3()
 
    return (
-      <div className='grid h-screen'>
+      <div className='grid h-screen w-screen text-white bg-slate-900'>
          <button onClick={()=>connectWallet('injected')}>Connect Wallet</button>
       </div>
    )
